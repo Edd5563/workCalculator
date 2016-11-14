2ec37cba14a6b12779c07d69bdf2dd15444f6503
edwin rivera <techedwin@aol.com>
2016-11-14 13:04:45 -0500

    new storage feature added

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-#ReadMe....
-Calculator for work
+//#ReadMe....
+// Calculator for work
+// Implementing a storage for daily count
+
+
+$("#addUser").on("click", function() {
+
+	// Capture User Inputs and store into variables
+var name = $("#nameinput").val();
+var email = $("#emailinput").val();
+var age = $("#ageinput").val();
+var comment = $("#commentinput").val();
+
+
+	// Console log each of the user inputs to confirm you are receiving them correctly
+console.log(name);
+console.log(email);
+console.log(age);
+console.log(comment);
+
+
+	// Output all of the new information into the relevant HTML sections
+$('#namedisplay').text(name);
+$('#emaildisplay').text(email);
+$('#agedisplay').text(age);
+$('#commentdisplay').text(comment);
+
+localStorage.clear();
+
+localStorage.setItem('name', name);
+localStorage.setItem('email', email);
+localStorage.setItem('age', age);
+localStorage.setItem('comment', comment);
+
+	// Don't refresh the page!
+	return false;
+	})
+localStorage.getItem('name');
+localStorage.getItem('email');
+localStorage.getItem('age');
+localStorage.getItem('comment');
+$('#recentMember').empty();
+$('#recentMember').append(localStorage.getItem('name'));
+$('#recentMember').append(localStorage.getItem('email'));
+$('#recentMember').append(localStorage.getItem('age'));
+$('#recentMember').append(localStorage.getItem('comment'));
+
+
+})
--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,6 +1,6 @@
 // Connected
 $(document).ready(function(){
-
+var dataCount = 0;
 var callCount = 0;
 var chatCount = 0;
 var total = 0;
@@ -24,6 +24,46 @@ $(".total").on("click", function(){
 	var total = callCount + chatCount;
 	$("#totalCount").text("Your total so far is " + total);
 });
+
+
+
+$(".storeBtn").on("click", function(){
+	localStorage.clear();
+	localStorage.setItem('calls',callCount);
+	localStorage.setItem('chats', chatCount);
+	localStorage.setItem('total', total);
+	return false;
+});
+
+
+$(".clearBtn").on("click", function(){
+	callCount = 0;
+	chatCount = 0;
+	total = 0;
+	$("#totalCount").text("Total Cleared");
+	$("#chatCount").text("Chats Cleared");
+	$("#callCount").text("Calls Cleared");
+	localStorage.clear();
+});
+
+
+function checkTotals() {
+	if (callCount == 0 && chatCount == 0 && total == 0) {
+		$("#totalCount").text("").append(localStorage.getItem('total'));
+		$("#chatCount").text("Press a button to start").append(localStorage.getItem('chats'));
+		$("#callCount").text("Press a button to start").append(localStorage.getItem('calls'));
+		
+		} else {
+
+		$("#totalCount").text("Previous Count ").append(localStorage.getItem('total'));
+		$("#chatCount").text("Previous Count ").append(localStorage.getItem('chats'));
+		$("#callCount").text("Previous Count ").append(localStorage.getItem('calls'));
+		}
+	}
+
+
+checkTotals();
+
 
 
 
